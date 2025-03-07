a6c42f878f34dcf2c061b7ece7fad83b367d0102
Ivan Tulaev <ivan.aks.msk@gmail.com>
2025-03-07 11:26:55 +0300

    chore: test build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import packageJson from './package.json'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
+const packageName = packageJson.name.split('/').pop() || packageJson.name
+
 
 export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/Graph.ts'),
-            name: 'Graph',
+            // name: 'Graph',
+            name: packageName,
+            // fileName: packageName,
             // the proper extensions will be added
             // fileName: 'graph',
         },
         rollupOptions: {},
     },
+    plugins: [
+        dts({ rollupTypes: true }),
+    ]
 })
